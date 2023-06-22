4cd3453b55b925fad3298d9e3430b6215e4828b2
fermarino <119690095+fermarino@users.noreply.github.com>
2023-06-22 13:03:34 -0300

    campo de busca

--- src/components/GamesList/GamesList.jsx
+++ src/components/GamesList/GamesList.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 import styles from '@/components/GamesList/GamesList.module.css'
 import Game from '../Game/Game'
 
 const GamesList = (props) => {
+  
+  const [search, setSearch] = useState('');
+
+  const lowerSearch = search.toLowerCase();
+  
+  const filteredGames = props.games.filter((game) => game.title.toLowerCase().includes(lowerSearch))
   return (
-    <ul className={styles.gamesList}>
-      {props.games.map((game) => (
-        <Game
-          key={game.id}
-          title={game.title}
-          genre={game.genre}
-          thumbnail={game.thumbnail}
-          short_description={game.short_description}
-        />
-      ))}
-    </ul>
+    <>
+    <input
+      type='text'
+      value={search}
+      onChange={(e) => setSearch (e.target.value)}
+    />
+      <ul className={styles.gamesList}>
+        {filteredGames.map((game) => (
+          <Game
+            key={game.id}
+            title={game.title}
+            genre={game.genre}
+            thumbnail={game.thumbnail}
+            short_description={game.short_description}
+          />
+        ))}
+      </ul>
+    </>
   )
 }
 
