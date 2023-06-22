b7590a6aa3fd99f75662bf573078d147ecfbe316
fermarino <119690095+fermarino@users.noreply.github.com>
2023-06-22 15:25:23 -0300

    gêneros filtrados

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -9,7 +9,7 @@ const Game = (props) => {
       <h2>{props.title}</h2>
       <h3>{props.genre}</h3>
       <Image src={props.thumbnail} alt={props.title} width={300} height={300}/>
-      <p>{props.short__description}</p>
+      <p>{props.short_description}</p>
     </li>
   )
 }
--- src/components/GamesList/GamesList.jsx
+++ src/components/GamesList/GamesList.jsx
@@ -5,19 +5,33 @@ import styles from '@/components/GamesList/GamesList.module.css'
 import Game from '../Game/Game'
 
 const GamesList = (props) => {
-  
+
   const [search, setSearch] = useState('');
 
   const lowerSearch = search.toLowerCase();
-  
+
   const filteredGames = props.games.filter((game) => game.title.toLowerCase().includes(lowerSearch))
+
+  const filteredGenres = props.games.reduce((genres, game) => {
+    if (!genres.includes(game.genre)) {
+      genres.push(game.genre);
+    }
+    return genres;
+  }, []);
+
   return (
     <>
-    <input
-      type='text'
-      value={search}
-      onChange={(e) => setSearch (e.target.value)}
-    />
+      <input
+        type='text'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {filteredGenres.map((genre) => (
+        <button key={genre}>
+          {genre}
+        </button>
+
+      ))}
       <ul className={styles.gamesList}>
         {filteredGames.map((game) => (
           <Game
